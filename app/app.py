40617ceb664184b8cab236ae7831a8542eb1a3ec
Sara Riker <SaraRiker@MacBook-Pro.local>
2021-10-01 14:10:58 -0700

    update app

--- app/app.py
+++ app/app.py
@@ -3,7 +3,11 @@ import pandas as pd
 import datetime as dt
 from cctc import clean_members, format, clean_infra
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+        page_title="Cape Cod Tech Council",
+        page_icon="chart_with_upwards_trend",
+        layout="wide",
+    )
 
 st.title("Cape Cod Tech Council")
 
@@ -25,6 +29,12 @@ if pwd == st.secrets["password"]:
 
         st.sidebar.markdown("Import Members CSV")
         uploaded_file = st.sidebar.file_uploader("Choose a file")
+
+        # st.sidebar.markdown("Import Group CSV")
+        # uploaded_file2 = st.sidebar.file_uploader("Choose aother file")
+
+        # st.sidebar.markdown("Import Infrastructure CSV")
+        # uploaded_file3 = st.sidebar.file_uploader("Choose a third file")
 
         expire_date = pd.to_datetime(st.sidebar.date_input('Pick a Expiration Date')).strftime(format)
         result =""
