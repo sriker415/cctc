e3ab923f8c7c4754d1d317f156a86680731fff46
Sara Riker <SaraRiker@MacBook-Pro.local>
2021-10-01 12:57:39 -0700

    fix bug

--- app/app.py
+++ app/app.py
@@ -12,33 +12,31 @@ pwd = text_input_container.text_input("Enter a password", type="password")
 
 if pwd == password:
     text_input_container.empty()
-    
-    
 
     # Create a page dropdown 
-    page = st.sidebar.selectbox("Menu", ["Home", "Membership"]) 
-    if page == "Home":
-        st.markdown("Welcome")
+    # page = st.sidebar.selectbox("Menu", ["Home", "Membership"]) 
+    # if page == "Home":
+    #     st.markdown("Welcome")
 
-    elif page == "Membership":
+    # elif page == "Membership":
         
-        def main():
-            st.sidebar.title("Input Parameteres")
-            expire_date = pd.to_datetime(st.sidebar.date_input('Pick a Expiration Date')).strftime(format)
-            result =""
-            
-            st.markdown("Import Members CSV")
-            uploaded_file = st.file_uploader("Choose a file")
-
-            if uploaded_file is not None:
-                members = pd.read_csv(uploaded_file)
-                members = clean_members(members)
-                members_filtered = members[members['Membership Expiration Date'] <= expire_date]
-
-                st.table(members_filtered)
+    def main():
+        st.sidebar.title("Input Parameteres")
+        expire_date = pd.to_datetime(st.sidebar.date_input('Pick a Expiration Date')).strftime(format)
+        result =""
         
-        if __name__=='__main__': 
-            main()
+        st.markdown("Import Members CSV")
+        uploaded_file = st.file_uploader("Choose a file")
+
+        if uploaded_file is not None:
+            members = pd.read_csv(uploaded_file)
+            members = clean_members(members)
+            members_filtered = members[members['Membership Expiration Date'] <= expire_date]
+
+            st.table(members_filtered)
+    
+    if __name__=='__main__': 
+        main()
     
 
 elif len(pwd) > 0:
